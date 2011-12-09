2a7b47d5cb1d8d2d455395a2273ba5a225006c8e
kalpesh <kalpesh@latitudetechnolabs.com>
2011-12-09 13:32:23 +0530

    sql changes in phase4

---
+++ lnut/application/sqlupdates/phase4.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `users_schools` CHANGE `contact` `contact` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+ALTER TABLE `users_schools` CHANGE `school` `school` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ;
