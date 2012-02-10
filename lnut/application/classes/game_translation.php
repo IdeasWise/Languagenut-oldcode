dbc5af458f1f94961a6f28150ef7baf119b6edd5
kalpesh <kalpesh@latitudetechnolabs.com>
2012-02-10 18:08:50 +0530

    game translation changes

--- lnut/application/classes/game_translation.php
+++ lnut/application/classes/game_translation.php
@@ -46,6 +46,9 @@ class game_translation extends generic_object {
 
 	public function updateGameTranslation() {
 		if (count($_POST) > 0) {
+			echo '<pre>';
+			print_r($_POST);
+			echo '</pre>';
 
 			foreach ($_POST as $key => $val) {
 				$name = explode('_', $key);
@@ -66,6 +69,9 @@ class game_translation extends generic_object {
 					$query.="AND ";
 					$query.="`language_uid`='" . mysql_real_escape_string($language_uid) . "' ";
 					$query.="LIMIT 1";
+					if($language_uid == 26) {
+						echo $query;
+					}
 					$result = database::query($query);
 					if ($result && mysql_error() == '') {
 						$row = mysql_fetch_array($result);
@@ -82,6 +88,10 @@ class game_translation extends generic_object {
 							$query.="`game_uid`='" . mysql_real_escape_string($game_uid) . "' ";
 							$query.="LIMIT 1";
 							$result = database::query($query);
+							if($language_uid == 26) {
+								echo '<br><br>';
+								echo $query;
+							}
 							if(mysql_error()!='') {
 								die($query.'<br><br>'.mysql_error());
 							}
@@ -98,6 +108,10 @@ class game_translation extends generic_object {
 							$query.="'" . mysql_real_escape_string($val) . "',";
 							$query.="'" . $instruction . "'";
 							$query.=")";
+							if($language_uid == 26) {
+								echo '<br><br>';
+								echo $query;
+							}
 							$result = database::query($query);
 							if(mysql_error()!='') {
 								die($query.'<br><br>'.mysql_error());
@@ -106,6 +120,7 @@ class game_translation extends generic_object {
 					}
 				}
 			}
+			exit;
 		}
 	}
 
